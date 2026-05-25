24092f506c80a8635f5cbf39ebee7efeacd3de13
Garima Saha <80147820+gari950@users.noreply.github.com>
2026-05-25 22:07:25 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (39.58%) - LeetSync

--- BinarySearch/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ BinarySearch/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -3,6 +3,9 @@
  * @return {number}
  */
 
+
+/*            TC -- O(N^2) SC -- O(N)
+
 var singleNonDuplicate = function (nums) {
     let mp = new Map();
     const getKey = (val) => {
@@ -13,3 +16,22 @@ var singleNonDuplicate = function (nums) {
     }
     return getKey(Math.min(...mp.values()));
 };
+*/
+
+var singleNonDuplicate = function (nums) {
+    let cnt = 0;
+    if (nums.length < 2) return nums[0];
+    for (let i = 0; i <= nums.length - 1; i += 2) {
+        // console.log(`${nums[i]}:${nums[(i + 1) % nums.length]}`)
+        if ((i + 1) < nums.length) {
+            if (nums[i] != nums[(i + 1) % nums.length]) {
+                cnt = nums[i];
+                break;
+            }
+        } else {
+            cnt = nums[i];
+            break;
+        }
+    }
+    return cnt;
+}
