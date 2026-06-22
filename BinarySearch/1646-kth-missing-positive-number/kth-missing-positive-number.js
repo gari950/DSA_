a70c5293a51665a54939942a326a7b444630df87
Garima Saha <80147820+gari950@users.noreply.github.com>
2026-06-22 22:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (87.50%) - LeetSync

--- BinarySearch/1646-kth-missing-positive-number/kth-missing-positive-number.js
+++ BinarySearch/1646-kth-missing-positive-number/kth-missing-positive-number.js
@@ -4,9 +4,23 @@
  * @return {number}
  */
 var findKthPositive = function (arr, k) {
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]<=k)k++;
-    else break;
-  }
-  return k;
+    //   for(let i=0;i<arr.length;i++){
+    //     if(arr[i]<=k)k++;
+    //     else break;
+    //   }
+    //   return k;
+
+
+    // console.log(missing)
+    let l = 0, r = arr.length - 1;
+
+    // console.log(r)
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        let missing = arr[mid] - (mid + 1);
+        if (missing < k) l = mid + 1;
+        else r = mid - 1;
+    }
+    // console.log(r, missing[r - 1])
+    return r+1+k;
 };
